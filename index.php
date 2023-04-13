11308f1b67b367d36bdaf5313728b2f44134f54e
Cloud <ruben.as532@gmail.com>
2023-04-13 18:41:29 +0200

    Apparently first complete version,minute 28

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
+declare(strict_types = 1);
 //TODO: Entrada de datos por terminal o formulario.
-$numbers = [1,2];
+$numbers = [1,4,7];
 if(checkArray($numbers)) {
-	echo "Ok,lets go!";
+	print_r(getLostNumbers($numbers));
 }
 else {
 	echo "Ordered integer array entered not valid. So sorry:(";
@@ -19,5 +20,19 @@ function checkArray(array $numbers): bool {
 	}
 	if(!is_int($numbers[count($numbers) - 1])) return false;
 	return true;
- }
+}
+
+function getLostNumbers(array $numbers): array {
+	$all_numbers = generateCompleteList($numbers[0],$numbers[count($numbers)-1]);
+	return array_diff($all_numbers,$numbers);
+}
+
+function generateCompleteList(int $begin, int $end): array {
+	$complete_list = array();
+	while($begin <= $end) {
+		array_push($complete_list, $begin);
+		++$begin;
+	}
+	return $complete_list;
+}
 ?>
